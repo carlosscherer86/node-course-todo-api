2e92707eb8d83fbfb0923f214e73476a3f94f7f7
carlosscherer86 <carloseduscherer@gmail.com>
2018-04-25 22:21:47 -0300

    Add DELETE /Users/me/token route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
+    debugger;
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
@@ -125,6 +126,14 @@ app.post('/users/login', (req, res) => {
     }).catch((err) => {
       res.status(400).send();
     });
+  });
+
+  app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
   });
 
 app.listen(port, () => {
